7bfa50ca1b10516c08533f453c706f7d59cac277
Imamsubekti26 <imam0subekti@gmail.com>
2023-05-09 11:58:57 +0700

    convert htmload from object to class, add feature

--- dist/htmloader.js
+++ dist/htmloader.js
@@ -7,111 +7,129 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const htmloader = {
-    /** temporary save the element selected by 'target' function */
-    _target: null,
-    /** temporary save the datas inputed by 'withData' function or any other function */
-    _data: [],
-    /**
-     * replace any element that have 'data-hl-load' attribute with component from other file
-     * @param {string} componentLocation  directory address that save the various components
-     */
-    useComponentIn: function (componentLocation) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const elements = document.querySelectorAll("[data-hl-load]");
-            for (let i = 0; i < elements.length; i++) {
-                const fileName = elements[i].getAttribute("data-hl-load");
-                const data = JSON.parse(elements[i].getAttribute("data-hl-use"));
-                const isOverride = elements[i].getAttribute("data-hl-override") === "false" ? false : true;
-                this._target = elements[i];
-                yield this.withData(data).render(`${componentLocation}/${fileName}.html`, isOverride);
-            }
-        });
-    },
-    /**
-     * Select element in the DOM
-     * @param   {string}  DomTarget element selector
-     * @returns {this}              return htmloader itself
-     */
-    target: function (DomTarget) {
-        this._target = document.querySelector(DomTarget);
-        return this;
-    },
-    /**
-     * saves the data you want to inject into the template
-     * @param   {array | object}  data  data you want to inject into the template
-     * @returns {this}                  return htmloader itself
-     */
-    withData: function (data) {
-        if (data === null || data === undefined)
-            return this;
-        if (Array.isArray(data)) {
-            this._data = data;
-            return this;
-        }
-        this._data.push(data);
-        return this;
-    },
-    /**
-     * render HTML component from other file to the DOM within or without data
-     * @param   {string}              componentUri  location of file that need to be rendered
-     * @param   {boolean}             isOverride    the DOM target will be replaced if true or appended if false, default false
-     * @returns {Promise<void | 0>}
-     */
-    render: function (componentUri, isOverride) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // get component
-            const getComponent = yield fetch(componentUri);
-            let component = yield getComponent.text();
-            // push _data with dummy data to make the component renderable
-            if (this._data.length === 0) {
-                this._data.push({
-                    dummy: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, recusandae.",
-                });
-            }
-            // render it
-            for (let i = 0; i < this._data.length; i++) {
-                let componentWithData = yield this.injectDataToString(component, this._data[i]);
-                if (isOverride) {
-                    yield this._target.parentNode.replaceChild(yield this.convertStringtoNode(componentWithData), this._target);
-                    this.resetInit();
-                    return 0;
+class HtmLoader {
+    /** set _target and _data with starter value */
+    constructor() {
+        /**
+         * replace any element that have 'data-hl-load' attribute with component from other file
+         * @param {string} componentLocation  directory address that save the various components
+         */
+        this.useComponentIn = function (componentLocation) {
+            return __awaiter(this, void 0, void 0, function* () {
+                const elements = document.querySelectorAll("[data-hl-load]");
+                for (let i = 0; i < elements.length; i++) {
+                    const fileName = elements[i].getAttribute("data-hl-load");
+                    const data = JSON.parse(elements[i].getAttribute("data-hl-use"));
+                    const isOverride = elements[i].getAttribute("data-hl-override") === "false" ? false : true;
+                    this._target = elements[i];
+                    yield this.withData(data).render(`${componentLocation}/${fileName}.html`, isOverride);
+                    elements[i].removeAttribute("data-hl-use");
+                    elements[i].removeAttribute("data-hl-override");
                 }
-                yield this._target.append(yield this.convertStringtoNode(componentWithData));
-                if (i >= this._data.length)
-                    this.resetInit();
-            }
-        });
-    },
-    /**
-     * inject data to string that have pattern "::variableName"
-     * @param   {Promise<string>}   text  text that you want to insert data into it
-     * @param   {object}            data  data that you want to insert to text
-     * @returns {Promise<string>}         text that has been inserted with data
-     */
-    injectDataToString: function (text, data) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let processedText = yield text;
-            Object.keys(data).forEach((key) => {
-                processedText = processedText.replace(`::${key}`, data[key]);
             });
-            return processedText;
-        });
-    },
-    /**
-     * convert text to HTML Node
-     * @param   {Promise<string>} text  text that needs to be converted
-     * @returns {Promise<Node>}         HTML node that ready to use
-     */
-    convertStringtoNode: function (text) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const wrapper = document.createElement("div");
-            const textResult = yield text;
-            wrapper.innerHTML = textResult.trim();
-            return wrapper.firstChild;
-        });
-    },
-    resetInit: function () {
-        (this._data = []), (this._target = null);
-    },
-};
+        };
+        /**
+         * Select element in the DOM
+         * @param   {string}  DomTarget element selector
+         * @returns {this}              return htmloader itself
+         */
+        this.target = function (DomTarget) {
+            const newThis = new HtmLoader();
+            newThis._target = document.querySelector(DomTarget);
+            return newThis;
+        };
+        /**
+         * saves the data you want to inject into the template
+         * @param   {array | object}  data  data you want to inject into the template
+         * @returns {this}                  return htmloader itself
+         */
+        this.withData = function (data) {
+            if (data === null || data === undefined)
+                return this;
+            if (Array.isArray(data)) {
+                this._data = data;
+                return this;
+            }
+            this._data.push(data);
+            return this;
+        };
+        /**
+         * render HTML component from other file to the DOM within or without data
+         * @param   {string}              componentUri  location of file that need to be rendered
+         * @param   {boolean}             isOverride    the DOM target will be replaced if true or appended if false, default false
+         * @returns {Promise<void | 0>}
+         */
+        this.render = function (componentUri, isOverride) {
+            return __awaiter(this, void 0, void 0, function* () {
+                // get component
+                const getComponent = yield fetch(componentUri);
+                let component = yield getComponent.text();
+                // push _data with dummy data to make the component renderable
+                if (this._data.length === 0) {
+                    this._data.push({
+                        dummy: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, recusandae.",
+                    });
+                }
+                // render it
+                yield this.doRender(component, isOverride);
+            });
+        };
+        /**
+         * inject data to string that have pattern "::variableName"
+         * @param   {Promise<string>}   text  text that you want to insert data into it
+         * @param   {object}            data  data that you want to insert to text
+         * @returns {Promise<string>}         text that has been inserted with data
+         */
+        this.injectDataToString = function (text, data) {
+            return __awaiter(this, void 0, void 0, function* () {
+                let processedText = yield text;
+                Object.keys(data).forEach((key) => {
+                    processedText = processedText.replace(`::${key}`, data[key]);
+                });
+                return processedText;
+            });
+        };
+        /**
+         * convert text to HTML Node
+         * @param   {Promise<string>} text  text that needs to be converted
+         * @returns {Promise<Node>}         HTML node that ready to use
+         */
+        this.convertStringtoNode = function (text) {
+            return __awaiter(this, void 0, void 0, function* () {
+                const wrapper = document.createElement("div");
+                const textResult = yield text;
+                wrapper.innerHTML = textResult.trim();
+                return wrapper.firstChild;
+            });
+        };
+        /**
+         * used by 'render' function
+         * @param   {string}                component   component that want to render
+         * @param   {boolean}               isOverride  the DOM target will be replaced if true or appended if false, default false
+         * @returns {Promise<void | 0>}
+         */
+        this.doRender = function (component, isOverride) {
+            return __awaiter(this, void 0, void 0, function* () {
+                for (let i = 0; i < this._data.length; i++) {
+                    let componentWithData = yield this.injectDataToString(component, this._data[i]);
+                    if (isOverride) {
+                        yield this._target.parentNode.replaceChild(yield this.convertStringtoNode(componentWithData), this._target);
+                        this.resetClass();
+                        return 0;
+                    }
+                    yield this._target.append(yield this.convertStringtoNode(componentWithData));
+                    if (i >= this._data.length)
+                        this.resetClass();
+                }
+            });
+        };
+        /**
+         * reset _data and _target to started value
+         */
+        this.resetClass = function () {
+            (this._data = []), (this._target = null);
+        };
+        this._target = null;
+        this._data = [];
+    }
+}
--- src/htmloader.ts
+++ src/htmloader.ts
@@ -1,27 +1,30 @@
-interface htmloader_object {
-  useComponentIn(componentLocation: string): Promise<void>;
-  target(DomTarget: string): this;
-  withData(data: Array<object> | object): this;
-  render(componentUri: string, isOverride?: boolean): Promise<void | 0>;
-  injectDataToString(text: Promise<string>, data: object): Promise<string>;
-  convertStringtoNode(text: Promise<string>): Promise<Node>;
-  resetInit(): void;
-  _target: Node;
+interface HL_Skeleton {
+  useComponentIn: (componentLocation: string) => Promise<void>;
+  target: (DomTarget: string) => object;
+  withData: (data: Array<object> | object) => object;
+  render: (componentUri: string, isOverride?: boolean) => Promise<void>;
+  _target: null | Node;
   _data: Array<object>;
 }
 
-const htmloader: htmloader_object = {
+class HtmLoader implements HL_Skeleton {
   /** temporary save the element selected by 'target' function */
-  _target: null,
+  _target: null | Node;
 
   /** temporary save the datas inputed by 'withData' function or any other function */
-  _data: [],
+  _data: Array<object>;
+
+  /** set _target and _data with starter value */
+  constructor() {
+    this._target = null;
+    this._data = [];
+  }
 
   /**
    * replace any element that have 'data-hl-load' attribute with component from other file
    * @param {string} componentLocation  directory address that save the various components
    */
-  useComponentIn: async function (componentLocation: string): Promise<void> {
+  useComponentIn = async function (componentLocation: string): Promise<void> {
     const elements: NodeListOf<Element> =
       document.querySelectorAll("[data-hl-load]");
 
@@ -37,34 +40,37 @@ const htmloader: htmloader_object = {
         `${componentLocation}/${fileName}.html`,
         isOverride
       );
+
+      elements[i].removeAttribute("data-hl-use");
+      elements[i].removeAttribute("data-hl-override");
     }
-  },
+  };
 
   /**
    * Select element in the DOM
    * @param   {string}  DomTarget element selector
    * @returns {this}              return htmloader itself
    */
-  target: function (DomTarget: string): htmloader_object {
-    this._target = document.querySelector(DomTarget);
-    return this;
-  },
+  target = function (DomTarget: string): object {
+    const newThis: HtmLoader = new HtmLoader();
+    newThis._target = document.querySelector(DomTarget);
+    return newThis;
+  };
 
   /**
    * saves the data you want to inject into the template
    * @param   {array | object}  data  data you want to inject into the template
    * @returns {this}                  return htmloader itself
    */
-  withData: function (data: Array<object> | object): htmloader_object {
+  withData = function (data: Array<object> | object): object {
     if (data === null || data === undefined) return this;
-
     if (Array.isArray(data)) {
       this._data = data;
       return this;
     }
     this._data.push(data);
     return this;
-  },
+  };
 
   /**
    * render HTML component from other file to the DOM within or without data
@@ -72,10 +78,10 @@ const htmloader: htmloader_object = {
    * @param   {boolean}             isOverride    the DOM target will be replaced if true or appended if false, default false
    * @returns {Promise<void | 0>}
    */
-  render: async function (
+  render = async function (
     componentUri: string,
     isOverride?: boolean
-  ): Promise<void | 0> {
+  ): Promise<void> {
     // get component
     const getComponent: Response = await fetch(componentUri);
     let component: string = await getComponent.text();
@@ -89,6 +95,50 @@ const htmloader: htmloader_object = {
     }
 
     // render it
+    await this.doRender(component, isOverride);
+  };
+
+  /**
+   * inject data to string that have pattern "::variableName"
+   * @param   {Promise<string>}   text  text that you want to insert data into it
+   * @param   {object}            data  data that you want to insert to text
+   * @returns {Promise<string>}         text that has been inserted with data
+   */
+  private injectDataToString = async function (
+    text: Promise<string>,
+    data: object
+  ): Promise<string> {
+    let processedText: string = await text;
+    Object.keys(data).forEach((key: string) => {
+      processedText = processedText.replace(`::${key}`, data[key]);
+    });
+    return processedText;
+  };
+
+  /**
+   * convert text to HTML Node
+   * @param   {Promise<string>} text  text that needs to be converted
+   * @returns {Promise<Node>}         HTML node that ready to use
+   */
+  private convertStringtoNode = async function (
+    text: Promise<string>
+  ): Promise<Node> {
+    const wrapper = document.createElement("div");
+    const textResult = await text;
+    wrapper.innerHTML = textResult.trim();
+    return wrapper.firstChild;
+  };
+
+  /**
+   * used by 'render' function
+   * @param   {string}                component   component that want to render
+   * @param   {boolean}               isOverride  the DOM target will be replaced if true or appended if false, default false
+   * @returns {Promise<void | 0>}
+   */
+  private doRender = async function (
+    component: string,
+    isOverride: boolean
+  ): Promise<void | 0> {
     for (let i = 0; i < this._data.length; i++) {
       let componentWithData: string = await this.injectDataToString(
         component,
@@ -99,46 +149,20 @@ const htmloader: htmloader_object = {
           await this.convertStringtoNode(componentWithData),
           this._target
         );
-        this.resetInit();
+        this.resetClass();
         return 0;
       }
       await this._target.append(
         await this.convertStringtoNode(componentWithData)
       );
-      if (i >= this._data.length) this.resetInit();
+      if (i >= this._data.length) this.resetClass();
     }
-  },
+  };
 
   /**
-   * inject data to string that have pattern "::variableName"
-   * @param   {Promise<string>}   text  text that you want to insert data into it
-   * @param   {object}            data  data that you want to insert to text
-   * @returns {Promise<string>}         text that has been inserted with data
+   * reset _data and _target to started value
    */
-  injectDataToString: async function (
-    text: Promise<string>,
-    data: object
-  ): Promise<string> {
-    let processedText: string = await text;
-    Object.keys(data).forEach((key: string) => {
-      processedText = processedText.replace(`::${key}`, data[key]);
-    });
-    return processedText;
-  },
-
-  /**
-   * convert text to HTML Node
-   * @param   {Promise<string>} text  text that needs to be converted
-   * @returns {Promise<Node>}         HTML node that ready to use
-   */
-  convertStringtoNode: async function (text: Promise<string>): Promise<Node> {
-    const wrapper = document.createElement("div");
-    const textResult = await text;
-    wrapper.innerHTML = textResult.trim();
-    return wrapper.firstChild;
-  },
-
-  resetInit: function (): void {
+  private resetClass = function (): void {
     (this._data = []), (this._target = null);
-  },
-};
+  };
+}
